408969e931a2ee00c92df1b418d5f3b25cd74139
lvalentine6 <ysr1227@naver.com>
2024-10-09 17:31:56 +0900

    refactor: Change StudyCafePassMachine field injection

--- src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
@@ -1,10 +1,22 @@
 package cleancode.studycafe.tobe;
 
+import cleancode.studycafe.tobe.io.InputHandler;
+import cleancode.studycafe.tobe.io.OutputHandler;
+import cleancode.studycafe.tobe.io.StudyCafeFileHandler;
+
 public class StudyCafeApplication {
 
-    public static void main(String[] args) {
-        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
-        studyCafePassMachine.run();
-    }
+	public static void main(String[] args) {
+		InputHandler inputHandler = new InputHandler();
+		OutputHandler outputHandler = new OutputHandler();
+		StudyCafeFileHandler studyCafeFileHandler = new StudyCafeFileHandler();
+
+		StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine(
+			inputHandler,
+			outputHandler,
+			studyCafeFileHandler);
+
+		studyCafePassMachine.run();
+	}
 
 }
